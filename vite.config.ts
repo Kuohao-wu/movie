37e362939797633cc2a0271e1182addc5abfc880
kuohao-wu <15015618196@163.com>
2024-09-12 22:09:33 +0800

    重新构建代码

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
   },
   server: {
     host: '0.0.0.0',
